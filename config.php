daab02fb5d8df9a8fb64e7d7d9dcb48338f1fbae
sparshbaurasi <107546419+sparshbaurasi@users.noreply.github.com>
2022-07-20 19:58:59 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+	// $servername = "db";
+	// $username = "banker";
+	// $password = "password";
+	// $dbname = "banksysphp";
+
+	// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	// if(!$conn){
+	// 	die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	// }
+
+
+	$connect = mysqli_connect(
+		'db',
+		'banker',
+		'password',
+		'banksysphp'
+	);
+
+
+?>
